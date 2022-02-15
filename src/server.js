db26b3d42c8974bfad49ea68f40335da23990bc9
FusiDaniel <fpires.daniel@gmail.com>
2022-02-14 21:54:22 -0300

    Final commit

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require("express")
 const server = express()
 
-const { pageLanding, pageStudy, pageGiveClasses } = require("./pages")
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require("./pages")
 
 // nunjucks config (template engine)
 const nunjucks = require("nunjucks")
@@ -13,10 +13,14 @@ nunjucks.configure("src/views", {
 
 
 server
+    // alteracao para o server receber os dados do req.body
+    .use(express.urlencoded({ extended:true }))
     // Static files config (cssm scriptsm images)
     .use(express.static("public"))
     // aplication routes
     .get("/", pageLanding)
     .get("/study", pageStudy)
     .get("/give-classes", pageGiveClasses)
+    .post("/save.classes", saveClasses)
+
     .listen(5500)
